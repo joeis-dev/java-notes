1a924c13d0d6abf6af0d7b5e444b8fc625bd9e58
Jose Garcia <josegarcia.dev@gmail.com>
2025-06-07 17:38:51 -0600

    feature: GreetingController was created.

---
+++ testing/src/main/java/dev/joeis/controller/GreetingController.java
@@ -0,0 +1,22 @@
+package dev.joeis.controller;
+
+import dev.joeis.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    private final GreetingService greetingService;
+
+    @Autowired
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @GetMapping("/greet")
+    public String greet(@RequestParam(required = false) String name) {
+        return greetingService.greet(name);
+    }
+}
